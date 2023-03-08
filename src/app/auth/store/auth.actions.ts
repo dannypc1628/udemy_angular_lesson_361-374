ee6c9d2b3bb1ce515be2eaa7303656cb69d3f1af
hsien <hoanplpyy@hotmail.com>
2023-03-08 23:12:25 +0800

    383-1. 增加取消訂閱，loading state和處理錯誤的設定

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -4,6 +4,7 @@ export const LOGIN_START = "[Auth] Login Start";
 export const AUTHENTICATE_FAIL = "[Auth] Login Fail";
 export const AUTHENTICATE_SUCCESS = "[Auth] Login";
 export const SIGNUP_START = "[Auth] Signup Start";
+export const CLEAR_ERROR = "[AUTH] Clear Error";
 export const LOGOUT = "[Auth] Logout";
 
 export class AuthenticateSuccess implements Action {
@@ -26,13 +27,13 @@ export class AuthenticateSuccess implements Action {
 
 export class LoginStart implements Action {
   readonly type = LOGIN_START;
-  constructor(public payload: { email: string; password: string }) {}
+  constructor(public payload: { email: string; password: string }) { }
 }
 
 export class AuthenticateFail implements Action {
   readonly type = AUTHENTICATE_FAIL;
 
-  constructor(public payload: string) {}
+  constructor(public payload: string) { }
 }
 
 export class Logout implements Action {
@@ -41,7 +42,11 @@ export class Logout implements Action {
 
 export class SignupStart implements Action {
   readonly type = SIGNUP_START;
-  constructor(public payload: { email: string; password: string }) {}
+  constructor(public payload: { email: string; password: string }) { }
+}
+
+export class ClearError implements Action {
+  readonly type = CLEAR_ERROR;
 }
 
 export type AuthActions =
@@ -49,4 +54,5 @@ export type AuthActions =
   | Logout
   | LoginStart
   | AuthenticateFail
-  | SignupStart;
+  | SignupStart
+  | ClearError;
