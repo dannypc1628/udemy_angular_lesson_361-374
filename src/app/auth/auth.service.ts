61061e7ce9d5a6bd110bd864e3e267e24fadbc89
hsien <hoanplpyy@hotmail.com>
2023-03-09 22:40:59 +0800

    385-2: 同時移除interface

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,24 +3,11 @@ import { Store } from "@ngrx/store";
 import { Injectable } from "@angular/core";
 import * as formApp from "../store/app.reducer";
 
-export interface AuthResponseData {
-  kind: string;
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-  registered?: boolean;
-}
-
 @Injectable({ providedIn: "root" })
 export class AuthService {
   private tokenExpirationTimer: any;
 
-  constructor(
-
-    private store: Store<formApp.AppState>
-  ) { }
+  constructor(private store: Store<formApp.AppState>) { }
 
 
   autoLogout(expirationDuration: number) {
@@ -28,6 +15,4 @@ export class AuthService {
       this.logout();
     }, expirationDuration);
   }
-
-
 }
